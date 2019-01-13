be59d951b0e5c7328b3b01822fd6a99b5eb10ebf
Séga. S <sega.sylla.biz@gmail.com>
2019-01-13 22:23:03 +0100

    Init repo for sharing

---
+++ src/fr/epsi/stream/ReaderTextApp.java
@@ -0,0 +1,30 @@
+package fr.epsi.stream;
+
+/**
+ * Sujet : Manipuler un flux de texte depuis un fichier.<br/>
+ * Indications :
+ * <ul>
+ * <li>Extraire toutes les lignes du fichier commençant par "Lorem".</li>
+ * <li>Compter et affficher le nombre de ligne trouvée</li>
+ * </ul>
+ * Résultat attendu : <br/>
+ * Lorem iconsectetur adipiscin [...]<br/>
+ * Lorem ipsum dolor sit amet, [...]<br/>
+ * Lorem ipsum lacus quis pulvi [...]<br/>
+ * Lorem sed lobortis non lacus [...]<br/>
+ * Lorem ut enim mi. Ut in mi e [...]<br/>
+ * Lorem cras porttitor sed nib [...]<br/>
+ * <br/>
+ * Nombre de lignes trouvées : 6
+ */
+public class ReaderTextApp {
+	
+	private static final String FILE_NAME = "resources/lorem.txt";
+	private static final String PATTERN = "Lorem";
+	private static int count = 0;
+	
+	public static void main( String... args ) {
+	
+	
+	}
+}
